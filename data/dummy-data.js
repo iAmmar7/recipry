2430c06aa50bb339a6390046afb294541133c787
Ammar Ansari <iammaransari@gmail.com>
2021-10-16 17:31:30 +0500

    generate random categories with random colors

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,14 +1,47 @@
 import Category from '../models/category';
 
-export const CATEGORIES = [
-  new Category('c1', 'Italian', '#f5428d'),
-  new Category('c2', 'Quick & Easy', '#f54242'),
-  new Category('c3', 'Hamburgers', '#f5a442'),
-  new Category('c4', 'German', '#f5d142'),
-  new Category('c5', 'Light & Lovely', '#368dff'),
-  new Category('c6', 'Exotic', '#41d95d'),
-  new Category('c7', 'Breakfast', '#9eecff'),
-  new Category('c8', 'Asian', '#b9ffb0'),
-  new Category('c9', 'French', '#ffc7ff'),
-  new Category('c10', 'Summer', '#47fced')
+const colors = [
+  '#41d95d',
+  '#f5a442',
+  '#47fced',
+  '#f5d142',
+  '#368dff',
+  '#f5428d',
+  '#9eecff',
+  '#f54242',
+  '#ffc7ff',
+  '#b9ffb0',
 ];
+
+const categories = [
+  'Breakfast',
+  'Asian',
+  'Italian',
+  'French',
+  'Hamburgers',
+  'Quick & Easy',
+  'Light & Lovely',
+  'German',
+  'Exotic',
+  'Summer',
+];
+
+const randomNumbers = (max) => {
+  const generatedNumbers = [];
+  let i = 0;
+  while (i < max) {
+    generatedNumbers.push(
+      (function generateUniqueNumber() {
+        const newNumber = Math.floor(Math.random() * (max - 0)) + 0;
+        if (generatedNumbers.includes(newNumber)) return generateUniqueNumber();
+        return newNumber;
+      })()
+    );
+    i++;
+  }
+  return generatedNumbers;
+};
+
+export const CATEGORIES = randomNumbers(10).map(
+  (item, index) => new Category(`c${index}`, categories[index], colors[item])
+);
